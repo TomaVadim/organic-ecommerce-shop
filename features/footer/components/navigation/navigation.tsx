aab46765492a6b4eaefa616492dfa637191a85a7
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-04-29 22:51:15 +0300

    chore: update components

--- features/footer/components/navigation/navigation.tsx
+++ features/footer/components/navigation/navigation.tsx
@@ -1,6 +1,19 @@
 import { BrandLogo } from "@/features/components/brand-logo/brand-logo";
+import { NavigationItem } from "@/features/footer/shared/interfaces/navigation-options";
 
-export const Navigation = () => {
+interface Props {
+  myAccountNavColumn: NavigationItem[];
+  helpsNavColumn: NavigationItem[];
+  proxyNavColumn: NavigationItem[];
+  categoriesNavColumn: NavigationItem[];
+}
+
+export const Navigation = ({
+  myAccountNavColumn,
+  helpsNavColumn,
+  proxyNavColumn,
+  categoriesNavColumn,
+}: Props): JSX.Element => {
   return (
     <section className="bg-gray-900">
       <div className="container py-14 grid grid-cols-12 gap-y-10 border-b border-b-gray-400">
@@ -29,171 +42,95 @@ export const Navigation = () => {
         </article>
 
         <nav className="row-start-2 col-start-1 col-span-6 md:row-start-1 md:col-start-5 md:col-span-2">
-          <div>
-            <h5 className="mb-5 text-medium font-medium text-white">
-              My Account
-            </h5>
+          {myAccountNavColumn.map(({ title, links }) => (
+            <div key={title}>
+              <h5 className="mb-5 text-medium font-medium text-white">
+                {title}
+              </h5>
 
-            <ul className="flex flex-col gap-3 text-small text-gray-400">
-              <li>
-                <a
-                  className="hover:text-white transition-colors duration-200 focus-visible:bg-transparent"
-                  href="#"
-                >
-                  My Account
-                </a>
-              </li>
-              <li>
-                <a
-                  className="hover:text-white transition-colors duration-200 focus-visible:bg-transparent"
-                  href="#"
-                >
-                  Order History
-                </a>
-              </li>
-              <li>
-                <a
-                  className="hover:text-white transition-colors duration-200 focus-visible:bg-transparent"
-                  href="#"
-                >
-                  Shoping Cart
-                </a>
-              </li>
-              <li>
-                <a
-                  className="hover:text-white transition-colors duration-200 focus-visible:bg-transparent"
-                  href="#"
-                >
-                  Wishlist
-                </a>
-              </li>
-            </ul>
-          </div>
+              <ul className="flex flex-col gap-3 text-small text-gray-400">
+                {links.map(({ title, href }) => (
+                  <li key={title}>
+                    <a
+                      className="hover:text-white transition-colors duration-200 focus-visible:bg-transparent"
+                      href={href}
+                    >
+                      {title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </nav>
 
         <nav className="row-start-2 col-span-6 col-start-7 md:col-start-7 md:row-start-1 md:col-span-2">
-          <div>
-            <h5 className="mb-5 text-medium font-medium text-white">Helps</h5>
+          {helpsNavColumn.map(({ title, links }) => (
+            <div key={title}>
+              <h5 className="mb-5 text-medium font-medium text-white">
+                {title}
+              </h5>
 
-            <ul className="flex flex-col gap-3 text-small text-gray-400">
-              <li>
-                <a
-                  className="hover:text-white transition-colors duration-200 focus-visible:bg-transparent"
-                  href="#"
-                >
-                  Contact
-                </a>
-              </li>
-              <li>
-                <a
-                  className="hover:text-white transition-colors duration-200 focus-visible:bg-transparent"
-                  href="#"
-                >
-                  Faqs
-                </a>
-              </li>
-              <li>
-                <a
-                  className="hover:text-white transition-colors duration-200 focus-visible:bg-transparent"
-                  href="#"
-                >
-                  Terms & Condition
-                </a>
-              </li>
-              <li>
-                <a
-                  className="hover:text-white transition-colors duration-200 focus-visible:bg-transparent"
-                  href="#"
-                >
-                  Privacy Policy
-                </a>
-              </li>
-            </ul>
-          </div>
+              <ul className="flex flex-col gap-3 text-small text-gray-400">
+                {links.map(({ title, href }) => (
+                  <li key={title}>
+                    <a
+                      className="hover:text-white transition-colors duration-200 focus-visible:bg-transparent"
+                      href={href}
+                    >
+                      {title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </nav>
 
         <nav className="row-start-3 col-start-1 col-span-6 md:row-start-1 md:col-start-9 md:col-span-2">
-          <div>
-            <h5 className="mb-5 text-medium font-medium text-white">Proxy</h5>
+          {proxyNavColumn.map(({ title, links }) => (
+            <div key={title}>
+              <h5 className="mb-5 text-medium font-medium text-white">
+                {title}
+              </h5>
 
-            <ul className="flex flex-col gap-3 text-small text-gray-400">
-              <li>
-                <a
-                  className="hover:text-white transition-colors duration-200 focus-visible:bg-transparent"
-                  href="#"
-                >
-                  About
-                </a>
-              </li>
-              <li>
-                <a
-                  className="hover:text-white transition-colors duration-200 focus-visible:bg-transparent"
-                  href="#"
-                >
-                  Shop
-                </a>
-              </li>
-              <li>
-                <a
-                  className="hover:text-white transition-colors duration-200 focus-visible:bg-transparent"
-                  href="#"
-                >
-                  Product
-                </a>
-              </li>
-              <li>
-                <a
-                  className="hover:text-white transition-colors duration-200 focus-visible:bg-transparent"
-                  href="#"
-                >
-                  Track Order
-                </a>
-              </li>
-            </ul>
-          </div>
+              <ul className="flex flex-col gap-3 text-small text-gray-400">
+                {links.map(({ title, href }) => (
+                  <li key={title}>
+                    <a
+                      className="hover:text-white transition-colors duration-200 focus-visible:bg-transparent"
+                      href={href}
+                    >
+                      {title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </nav>
 
         <nav className="row-start-3 col-start-7 col-span-6 md:row-start-1 md:col-start-11 md:col-span-2">
-          <div>
-            <h5 className="mb-5 text-medium font-medium text-white">
-              Categories
-            </h5>
+          {categoriesNavColumn.map(({ title, links }) => (
+            <div key={title}>
+              <h5 className="mb-5 text-medium font-medium text-white">
+                {title}
+              </h5>
 
-            <ul className="flex flex-col gap-3 text-small text-gray-400">
-              <li>
-                <a
-                  className="hover:text-white transition-colors duration-200 focus-visible:bg-transparent"
-                  href="#"
-                >
-                  Fruit & Vegetables
-                </a>
-              </li>
-              <li>
-                <a
-                  className="hover:text-white transition-colors duration-200 focus-visible:bg-transparent"
-                  href="#"
-                >
-                  Meat & Fish
-                </a>
-              </li>
-              <li>
-                <a
-                  className="hover:text-white transition-colors duration-200 focus-visible:bg-transparent"
-                  href="#"
-                >
-                  Bread & Bakery
-                </a>
-              </li>
-              <li>
-                <a
-                  className="hover:text-white transition-colors duration-200 focus-visible:bg-transparent"
-                  href="#"
-                >
-                  Beauty & Health
-                </a>
-              </li>
-            </ul>
-          </div>
+              <ul className="flex flex-col gap-3 text-small text-gray-400">
+                {links.map(({ title, href }) => (
+                  <li key={title}>
+                    <a
+                      className="hover:text-white transition-colors duration-200 focus-visible:bg-transparent"
+                      href={href}
+                    >
+                      {title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </nav>
       </div>
     </section>
